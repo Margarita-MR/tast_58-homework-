a7c680ea1c8d290d7ddbbfc2e79f7aeb0bf63bf6
MargaritaM <ya.margarita1005@gmail.com>
2022-11-11 20:27:31 +0300

    Готово

--- Program.cs
+++ Program.cs
@@ -10,32 +10,28 @@
 // 2. Вычисление произведения матриц
 // 3. Вывод всех матриц
 
-// 1. Зададим 2 матрицы
 
 
-int [,] matrix1 = new int[2, 2];
-int [,] matrix2 = new int[2, 2];
+int [,] matrix1 = new int[3, 3];
+int [,] matrix2 = new int[3, 3];
 Random random = new Random();
 
 for (int i = 0; i < matrix1.GetLength(0); i++)
 {
     for (int j = 0; j < matrix1.GetLength(1); j++)
-    matrix1[i, j] = random.Next(5);
+    matrix1[i, j] = random.Next(10);
         {    
         for (int j = 0; j < matrix1.GetLength(1); j++)
         Console.Write(matrix1[i, j] + " ");
         Console.Write(" | | ");
             
     for (int j = 0; j < matrix2.GetLength(1); j++)
-    matrix2[i, j] = random.Next(5);
+    matrix2[i, j] = random.Next(10);
         for (int j = 0; j < matrix2.GetLength(1); j++)
         Console.Write(matrix2[i, j] + " ");
         Console.WriteLine();
         }
 }
-
-// 2. Вычисление произведения матриц
-// public static Matrix umn(Matrix matrix1, )
 
 int[,] UmnMatrix = Multiplication(matrix1, matrix2);
             Console.WriteLine("Произведение матриц:");
@@ -43,14 +39,14 @@ int[,] UmnMatrix = Multiplication(matrix1, matrix2);
             {
                 for (int j = 0; j < UmnMatrix.GetLength(1); j++)
                 {
-                    Console.Write(UmnMatrix[i, j]);
+                    Console.Write(UmnMatrix[i, j] + " ");
                 }
                 Console.WriteLine(" ");
             }
  
-static int[,] Multiplication(int[,] a, int[,] b)
+ static int[,] Multiplication(int[,] a, int[,] b)
 {
-    if(a.GetLength(1) !=b.GetLength(0)) throw new Exception("Число строк и столбцов должно совпадать");
+    if(a.GetLength(1) != b.GetLength(0)) throw new Exception("Число строк и столбцов должно совпадать");
     int[,] r = new int[a.GetLength(0), b.GetLength(1)];
     for (int i = 0; i < a.GetLength(0); i++)
     {
